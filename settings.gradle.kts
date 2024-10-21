938d3dba87a06ee8febc55482ca45e2086c628dd
Shivang Shah <sshah@block.xyz>
2024-10-21 11:08:50 -0400

    create sample projects :a and a:b

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 rootProject.name = "reproducer-project"
+include("a")
+include("a:b")
+findProject(":a:b")?.name = "b"
